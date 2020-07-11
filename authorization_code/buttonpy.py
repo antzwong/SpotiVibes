4956cc3604d75d0c8b1fad107eee63db8b2437e6
williampark1216 <66041900+williampark1216@users.noreply.github.com>
2020-07-11 12:35:13 -0700

    button py

---
+++ authorization_code/buttonpy.py
@@ -0,0 +1,19 @@
+import os
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
+
+# Configure session to use filesystem (instead of signed cookies)
+app.config["SESSION_FILE_DIR"] = mkdtemp()
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.route("/emotion", methods=["GET", "POST"])
+
+def emotion():
+    if request.method == "POST":
+        emotion = request.form.get("emotion")
+        
